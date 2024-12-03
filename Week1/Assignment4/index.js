e545282960dbd143629dbff8672383f68cbd37e6
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-03 10:47:50 +0200

    Assign4

---
+++ Week1/Assignment4/index.js
@@ -0,0 +1,18 @@
+console.group("Group 1");
+console.log("Message 1");
+console.log("Message 2");
+
+console.group("Child Group");
+console.log("Message 1");
+console.log("Message 2");
+
+console.group("Grand Child Group");
+console.log("Message 1");
+console.log("Message 2");
+
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group2");
+console.log("Message One");
+console.log("Message Two");
