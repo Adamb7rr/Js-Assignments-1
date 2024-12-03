d18ea98e130b705f627468e212b4b89ee8d0ab24
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-03 10:34:42 +0200

    Assign3

---
+++ Week1/Assignment3/index.js
@@ -0,0 +1,5 @@
+console.log('%celzero %cweb %cschool',
+            'font-size: 40px; color: red;',
+            'font-size: 40px; color: green; font-weight: bold;',
+            'font-size: 40px; background-color: blue; color: #FFF;'
+);
