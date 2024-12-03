39b8daf3085487e482e1c58f8839dd678b35cff7
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-03 10:26:43 +0200

    Assign2

---
+++ Week1/Assignment2/index.js
@@ -0,0 +1,6 @@
+document.write('<h1>Elzero</h1>');
+let Elzero = document.getElementsByTagName('h1')[0];
+Elzero.style.color = "blue";
+Elzero.style.fontSize = "40px";
+Elzero.style.textAlign = "center";
+Elzero.style.fontFamily = "Arial";
