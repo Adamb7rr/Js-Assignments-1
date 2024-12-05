fa0b7722d1b462d2f2ac0cff809267e24fb37628
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-05 20:17:08 +0200

    Assign3

---
+++ Week3/Assignment3/main.js
@@ -0,0 +1,13 @@
+let num = "10";
+
+// Solution One
+console.log(+num + 10); // 20
+
+// Solution Two
+console.log(+num * 2); // 20
+
+// Solution Three
+console.log(-num + 30); // 20
+
+// Solution Four
+console.log(Number(num) + Number(num)); // 20
