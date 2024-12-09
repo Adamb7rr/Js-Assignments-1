f0d262fe3a94bfeae41d01764a8a8ca776dc9a22
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-09 08:15:30 +0200

    Assign1

---
+++ Week4/Assignment1/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(50000 * 2); // 100000
+console.log(200000 / 2); // 100000
+console.log(Number('100000')); // 100000
+console.log(10 ** 5); // 100000
+console.log(Math.floor(1e5 + 0.5)); // 100000
+console.log(Math.ceil(1e5 - 0.5)); // 100000
+console.log(Math.round(1e5 + 0.2)); // 100000
+console.log(Number.parseInt("100000 Ali")); // 100000
+console.log(+"100000.0000"); // 100000
+console.log(Math.abs(-"100000")); // 100000
