a6c13b24d215c5429506ecaf88f73fe1cfb84adc
AdamAbdelafeth <sarahb7r@gmail.com>
2024-12-11 10:34:22 +0200

    Assign1

--- main.js
+++ main.js
@@ -42,8 +42,55 @@
 // console.log(+a + b + c)
 // console.log(a + b + c)
 
-let a = 10;
-let b = '20';
-let c = 80;
-console.log(++a + +b++ + +c++ - +a++) // 11 + 20 + 80 - 11 = 102
-console.log(++a) // 11 - 20 + 10 + 11 + 10 = 102
+// let a = 10;
+// let b = '20';
+// let c = 80;
+// console.log(++a + +b++ + +c++ - +a++) // 11 + 20 + 80 - 11 = 102
+// console.log(++a) // 11 - 20 + 10 + 11 + 10 = 102
+
+// let price = 100;
+// let discount = true;
+// let discountAmount = 20;
+
+// // if (discount === true) {
+// //     price -= discountAmount;
+// // }
+
+
+// discount ? price -= discountAmount : price;
+// console.log(price);
+
+// let day = 6;
+
+// switch (day) {
+//     case 1:
+//         document.write(`Today is Saturday.`);
+//         break;
+
+//     case 2:
+//         document.write('Today is Sunday.');
+//         break;
+
+//     case 3:
+//         document.write('Today is Monday.');
+//         break;
+
+//     case 4:
+//         document.write('Today is Thuesday.');
+//         break;
+
+//     case 5:
+//         document.write('Today is Wednesday.');
+//         break;
+
+//     case 6:
+//         document.write('Today is Thursday.');
+//         break;
+    
+//     case 7:
+//         document.write('Today is Friday.');
+//         break;
+    
+//     default:
+//         document.write('Unknown Day.')
+// }
